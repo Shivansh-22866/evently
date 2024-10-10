688b4974db55aada5e81affac5691d65ca78689e
Shivansh-22866 <shivanshp0418@gmail.com>
2024-10-10 23:31:20 +0530

    Related Events by Category done

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,13 +1,20 @@
-import { getEventById } from '@/lib/actions/event.actions'
+import Collection from '@/components/shared/Collection'
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils'
 import { SearchParamProps } from '@/types'
 import Image from 'next/image'
 import React from 'react'
 
-const EventDetails = async ({params: {id}}: SearchParamProps) => {
+const EventDetails = async ({params: {id}, searchParams}: SearchParamProps) => {
   const event = await getEventById(id)
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string
+  })
   console.log(event.organizer)
   return (
+    <>
     <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain' >
         <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
             <Image src={event.imageUrl} alt="hero image" width={1000} height={1000} className='h-full min-h-[300px] object-cover object-center' />
@@ -58,6 +65,19 @@ const EventDetails = async ({params: {id}}: SearchParamProps) => {
             </div>
         </div>
     </section>
+    <section className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+        <h2 className='h2-bold'>Related Events</h2>
+        <Collection
+          data = {relatedEvents?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
+    </section>
+    </>
   )
 }
 
