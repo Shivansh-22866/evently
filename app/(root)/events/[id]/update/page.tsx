80f52a2282ad35c6099b42a2bf3a2fcd88da0617
Shivansh-22866 <shivanshp0418@gmail.com>
2024-10-10 23:11:12 +0530

    Editing EventForms done

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,24 +1,43 @@
-import EventForm from "@/components/shared/EventForm";
+import EventForm from "@/components/shared/EventForm"
+import { getEventById } from "@/lib/actions/event.actions"
+import { getUser } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs/server";
-import React from "react";
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
 
-  const {userId} : {userId: string | null} = auth()
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  const {userId} = auth()
+  
+  // Get the MongoDB user document that matches the Clerk ID
+  let mongoUser = null
+  if (userId) {
+    mongoUser = await getUser(userId)
+  }
+  const event = await getEventById(id)
+
+  console.log(event)
+  console.log(userId)
 
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">
-          Create Event
-        </h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId!} type="Update" />
+        <EventForm 
+          type="Update" 
+          event={event}
+          eventId={event._id}
+          userId={mongoUser?._id} 
+        />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default UpdateEvent;
+export default UpdateEvent
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { CreateEventParams, DeleteEventParams, GetAllEventsParams } from "@/types"
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams, UpdateEventParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
@@ -83,6 +83,28 @@ export async function deleteEvent({ eventId, path }: DeleteEventParams) {
   
       const deletedEvent = await Event.findByIdAndDelete(eventId)
       if (deletedEvent) revalidatePath(path)
+    } catch (error) {
+      handleError(error)
+    }
+  }
+
+  export async function updateEvent({ userId, event, path }: UpdateEventParams) {
+    try {
+      await connectToDatabase()
+  
+      const eventToUpdate = await Event.findById(event._id)
+      if (!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
+        throw new Error('Unauthorized or event not found')
+      }
+  
+      const updatedEvent = await Event.findByIdAndUpdate(
+        event._id,
+        { ...event, category: event.categoryId },
+        { new: true }
+      )
+      revalidatePath(path)
+  
+      return JSON.parse(JSON.stringify(updatedEvent))
     } catch (error) {
       handleError(error)
     }
